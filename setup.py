a8fc445194b2faa1896251ef45ba21d482295f6d
Barry Stokman <barry.stokman@gmail.com>
2018-06-19 13:58:58 +0200

    bumped version 0.1 -> 1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='bioinfo_package',
-      version='0.1',
+      version='1.0',
       py_modules=['cli.biocli_module'],
       install_requires=['Click'],
       entry_points={'console_scripts': ['bioinfo=cli.biocli_module:biocli']},
