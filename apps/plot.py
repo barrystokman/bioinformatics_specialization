f82580153243190e2256e89422a081a8dafb4be5
Barry Stokman <barry.stokman@gmail.com>
2019-05-29 15:44:59 +0200

    add organism to title and optimizition changes

--- apps/plot.py
+++ apps/plot.py
@@ -5,8 +5,12 @@ from bioinformatics_1.functions import skew, minimum_skew
 class SkewPlot:
 
     def __init__(self, genome):
-        self.genome = genome
-        self.skew = skew(genome)
+        self.genome = genome.genome
+        self.organism_name = genome.file_name.split('.')[0].replace('_', ' ')
+
+    @property
+    def skew(self):
+        return skew(self.genome)
 
     @property
     def position_axis(self):
@@ -29,15 +33,19 @@ class SkewPlot:
         return [self.skew[position] for position in self.minimum_skew_positions]
 
     def generate_plot(self):
-        plt.plot(self.position_axis, self.skew_axis, 'b')
-        plt.xlim([0, len(self.position_axis)])
-        plt.title('Skew Diagram')
+        position_axis = self.position_axis
+        skew_axis = self.skew_axis
+        plt.plot(position_axis, skew_axis, 'b')
+        plt.xlim([0, len(position_axis)])
+        plt.title(f'Skew Diagram for {self.organism_name}')
         plt.xlabel('position')
         plt.ylabel('skew')
 
     def show_minimum_skew(self):
-        plt.plot(self.minimum_skew_positions, self.minimum_skew_values, 'ro')
-        for x, y in zip(self.minimum_skew_positions, self.minimum_skew_values):
+        minimum_skew_positions = self.minimum_skew_positions
+        minimum_skew_values = self.minimum_skew_values
+        plt.plot(minimum_skew_positions, minimum_skew_values, 'ro')
+        for x, y in zip(minimum_skew_positions, minimum_skew_values):
             plt.text(x, y, f"{x}, {y}")
 
     def show_plot(self):
